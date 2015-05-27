7274ef8ada7f49e291ff8a278ac9c60125b842a8
thuchobiet <thuchobiet@gmail.com>
2015-05-27 15:17:15 +0700

    init

---
+++ CaBlog/App_Start/BundleConfig.cs
@@ -0,0 +1,30 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace CaBlog
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery.js",
+                        "~/Scripts/jquery.fittext.js",
+                        "~/Scripts/jquery.mousewheel-3.0.6.pack.js",
+                        "~/Scripts/jquery.easing.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.min.js",
+                      "~/Scripts/respond.js",
+                      "~/Scripts/wow.min.js",
+                      "~/Scripts/creative.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.min.css",
+                      "~/font-awesome/css/font-awesome.min.css",
+                      "~/Content/animate.min.css",
+                      "~/Content/creative.css"));
+        }
+    }
+}
---
+++ CaBlog/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CaBlog.MvcApplication" Language="C#" %>
