7bf621178af361f7e4122e46a74de9c71634b19d
thuchobiet <thuchobiet@gmail.com>
2015-06-03 17:39:18 +0700

    update admin site

---
+++ CaBlog/Startup.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.AspNet.Identity;
+using System.Security.Claims;
+
+[assembly: OwinStartup(typeof(CaBlog.Startup))]
+
+namespace CaBlog
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //System.Web.Helpers.AntiForgeryConfig.UniqueClaimTypeIdentifier = ClaimTypes.Email;
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Admin/Login"),
+                CookieSecure = CookieSecureOption.SameAsRequest
+            });
+        }
+    }
+}
